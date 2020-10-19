4d7f5d8f0928a1cb1090330c9fc63d0caa733a96
Victoria Smith <victoriasmithh428@gmail.com>
2020-10-19 11:16:05 -0400

    shema created

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT
+    AUTO_INCREMENT NOT NULL,
+        burger_name VARCHAR
+    (30) NOT NULL,
+        devoured, 
+        PRIMARY KEY
+    (id)
+    )
